7bb13fbc10f4734d56b8a5ed585196e766e248a9
Aleix Soler <aleixsoler64@gmail.com>
2024-08-28 20:43:24 +0200

    feat: configurations for the bundler

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,50 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+import external from "rollup-plugin-peer-deps-external";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/bundle.js",
-      format: "cjs",
-    },
-    {
-      file: "dist/bundle.es.js",
-      format: "es",
-      exports: "named",
-    },
-  ],
-  plugins: [
-    babel({ babelHelpers: "bundled" }), // WARNING: No estic segur que em faci falta
-    resolve({
-      // pass custom options to the resolve plugin
-      moduleDirectories: ["node_modules"],
-    }),
-  ],
-  // indicate which modules should be treated as external
-  external: ["react"],
-};
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "mui-sidebar"
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        exports: "named",
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      babel({ babelHelpers: "bundled" }), // WARNING: No estic segur que em faci falta
+      resolve({
+        // pass custom options to the resolve plugin
+        moduleDirectories: ["node_modules"],
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      external()
+    ],
+    // indicate which modules should be treated as external
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.types,
+        format: "es",
+      },
+    ],
+    plugins: [dts.default()]
+  },
+];
